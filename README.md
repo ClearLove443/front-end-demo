8dd94f72e99965435083919a0ae63c540b688301
ClearLove443 <1127280933@qq.com>
2021-12-26 14:47:02 +0800

    init

---
+++ README.md
@@ -0,0 +1,29 @@
+# A Practical Guide to TypeScript - How to Build a Pokedex app using HTML, SCSS, TypeScript and nodejs express
+
+TypeScript is a superset that needs to compile to plain JavaScript. It offers more control over your code since it uses type annotations, interfaces, classes, and static type checking to throw errors at compile-time. TypeScript helps to enhance code quality and understandability, especially with a large codebase.
+
+In this guide, I will lead you through TypeScript by learning first all the basics needed to start with this great language, and then finish up this article by building an app from scratch using HTML, SCSS, and TypeScript.
+
+## The back end uses the nodejs express framework.
+
+## Compilation
+
+### typescript
+
+- Manual compilation
+
+```bash
+npm run tsc
+```
+
+- Automatic compilation
+
+```bash
+npm run gulp
+```
+
+## deploy
+
+```bash
+npm i && npm run gulp
+```
---
+++ gulpfile.js
@@ -0,0 +1,45 @@
+const gulp = require("gulp");
+const sass = require("gulp-sass")(require("sass"));
+const ts = require("gulp-typescript");
+const tsProject = ts.createProject("tsconfig.json");
+
+const paths = {
+  pages: ["src/*.html"],
+};
+gulp.task("html", () => {
+  return gulp.src(paths.pages).pipe(gulp.dest("dist"));
+});
+
+gulp.task("sass", (cb) => {
+  gulp
+    .src("src/*.scss")
+    .pipe(sass())
+    .pipe(
+      gulp.dest((f) => {
+        return f.base;
+      })
+    )
+    .pipe(gulp.dest("dist"));
+  cb();
+});
+
+gulp.task("typescript", () => {
+  return tsProject.src().pipe(tsProject()).js.pipe(gulp.dest("dist"));
+});
+
+// gulp 3
+// gulp.task("default", ["copy-html"], bundle);
+
+// 按照顺序执行
+// gulp.task("default", gulp.series("copy-html", "bundle"));
+
+// 并行执行
+gulp.task(
+  "default",
+  gulp.parallel("html", "sass", "typescript", (cb) => {
+    gulp.watch("src/*.html", gulp.series("html"));
+    gulp.watch("src/*.scss", gulp.series("sass"));
+    gulp.watch("src/*.ts", gulp.series("typescript"));
+    cb();
+  })
+);
